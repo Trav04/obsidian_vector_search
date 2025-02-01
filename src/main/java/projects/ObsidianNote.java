2eb185843f673b39c2a09266320d49d869652b6d
Travis <tngraham2004@gmail.com>
2025-02-01 15:21:11 +1000

    Implement ObsidianNote.java

--- src/main/java/projects/ObsidianNote.java
+++ src/main/java/projects/ObsidianNote.java
@@ -1,4 +1,20 @@
 package projects;
 
 public class ObsidianNote {
+
+    private String fileName;
+    private String id;
+
+    public ObsidianNote(String fileName, String id) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
